f0be68987210af063e63fd55808d7f39e1e845c7
sambhav6496 <jainsam620@gmail.com>
2021-01-31 11:20:24 +0530

    calculator

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ let equal = $('.equal')
 let clear = $('.clear')
 let userInput = true
 
-let number1Value = 0;
-let number2Value = 0;
-let operatorFlag = false;
+let number1Value = "";
 let operatorValue = undefined;
 
 getNumber()
@@ -15,63 +13,27 @@ getOperator()
 function getNumber(){
     number.click(function(e){
         numId = e.target.id
-        if(!operatorFlag){
-            if(userInput){
-                number1Value = numId.toString()
-                userInput = false
-            }else{
-                number1Value = number1Value + numId
-            }
-            display.text(`${number1Value}`)
-        }else{
-            if(userInput){
-                number2Value = numId.toString()
-                userInput = false
-            }else{
-                number2Value = number2Value + numId
-            }
-            display.text(`${number2Value}`)
-        }
-    })
+        number1Value = number1Value + numId
+        display.text(`${number1Value}`)
+})
 }
+
 
 function getOperator(){
     operator.click(function(e){
         operatorValue = e.target.id
-        console.log(operatorValue)
-        operatorFlag = true
-        userInput = true
+        number1Value = number1Value + operatorValue
+        display.text(`${number1Value}`)
     })
 }
 
 equal.click(function(){
-    performOperation(number1Value, number2Value, operatorValue)
+    let result = eval(number1Value)
+    display.text(`${result}`)
+    number1Value = ""
 })
 
-function performOperation(number1Input, number2Input, operatorInput){
-    let result = 0
-    number1Input = parseFloat(number1Input)
-    number2Input = parseFloat(number2Input)
-    if(operatorInput === "+"){
-        result = number1Input + number2Input
-    }else if(operatorInput === "-"){
-        result = number1Input - number2Input
-    }else if(operatorInput === "*"){
-        result = number1Input * number2Input
-    }else if(operatorInput === "/"){
-        result = number1Input / number2Input
-    }
-    display.text(`${result}`)
-    number1Value = result
-    number2Value = 0
-    userInput = true
-}
 
 clear.click(function(){
-    userInput = true
-    number1Value = 0;
-    number2Value = 0;
-    operatorFlag = false;
-    operatorValue = undefined;
-    display.text(`${number1Value}`)
+    number1Value =""
 })
